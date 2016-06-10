79e73a8eb6defbe4a859299f285687baed3ddca5
Duane Johnson <duane.johnson@gmail.com>
2016-06-10 10:36:02 -0600

    rescue bad references

--- src/biblerefs.rb
+++ src/biblerefs.rb
@@ -11,21 +11,21 @@ def bom_verse(book, chapter, verse)
 end
 
 def find_bom_file(book, chapter)
-	"../#{book.downcase.gsub(' ', '')}/chapter_#{'%02d' % chapter}.md"
+  "../#{book.downcase.gsub(' ', '')}/chapter_#{'%02d' % chapter}.md"
 end
 
 def inserted_before_verse(book, chapter, verse, text, highlight=true)
-	file = find_bom_file(book, chapter)
-	file_content = File.read(file)
-	replacement = highlight ? "#{text}\n\\1 {% em %}\\2{% endem %}" : "#{text}\n\\1 \\2"
-	file_content.sub(/^(\*\*#{book} #{chapter}:#{verse}\*\*) (.*)$/, replacement)
+  file = find_bom_file(book, chapter)
+  file_content = File.read(file)
+  replacement = highlight ? "#{text}\n\\1 {% em %}\\2{% endem %}" : "#{text}\n\\1 \\2"
+  file_content.sub(/^(\*\*#{book} #{chapter}:#{verse}\*\*) (.*)$/, replacement)
 end
 
 def kjv_margin_quote(book, chapter, verse)
-	body = $bible.book(book).chapter(chapter).verse(verse).text
-	pericope = "#{book} #{chapter}:#{verse}"
-	link = "http://www.kingjamesbibleonline.org/#{book}-Chapter-#{chapter}/"
-	"{% marginal %}\n> #### Quote::#{body}\n> KJV Bible, 1769, [#{pericope}](#{link})\n{% endmarginal %}"
+  body = $bible.book(book).chapter(chapter).verse(verse).text
+  pericope = "#{book} #{chapter}:#{verse}"
+  link = "http://www.kingjamesbibleonline.org/#{book}-Chapter-#{chapter}/"
+  "{% marginal %}\n> #### Quote::#{body}\n> KJV Bible, 1769, [#{pericope}](#{link})\n{% endmarginal %}"
 end
 
 blank_verse_format = lambda{ |b,c,v| '' }
@@ -40,16 +40,22 @@ BomDB.db[:refs].
     bible_pericope = "#{r[:ref_book]} #{r[:ref_chapter]}:#{r[:ref_verse]}"
     bom_pericope = "#{r[:book_name]} #{r[:verse_chapter]}:#{r[:verse_number]}"
     puts "#{bible_pericope} => #{bom_pericope}"
-    file = find_bom_file(r[:book_name], r[:verse_chapter])
-    puts file
 
-    # Put a marginal quotation of the bible reference next to the corresponding Book of Mormon verse
-    transformed = inserted_before_verse(r[:book_name], r[:verse_chapter], r[:verse_number],
-    	kjv_margin_quote(r[:ref_book], r[:ref_chapter], r[:ref_verse]))
+    begin
+      file = find_bom_file(r[:book_name], r[:verse_chapter])
+      puts file
+
+      # Put a marginal quotation of the bible reference next to the corresponding Book of Mormon verse
+      transformed = inserted_before_verse(r[:book_name], r[:verse_chapter], r[:verse_number],
+        kjv_margin_quote(r[:ref_book], r[:ref_chapter], r[:ref_verse]))
+    rescue Kj::Iniquity => e
+      # skip it
+      puts "Skipping due to error (#{e})"
+    end
 
     # Save the chapter with the added biblical reference
     File.open(file, "w") do |f|
-    	f.write(transformed)
+      f.write(transformed)
     end
   end
 
